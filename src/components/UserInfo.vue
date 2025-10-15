3eaf1d69581e040b3dd12fb0c9fe7d99884b9998
YuWeiZhang <2090423764@qq.com>
2025-10-15 17:35:30 +0800

    用户详情页，展示用户信息

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
         <h2>{{ username }}的个人中心</h2>
+        
+        <div class="info">
+            <p>姓名: {{ name }}</p>
+            <p>性别: {{ gender }}</p>
+            <p>类别: {{ type }}</p>
+            <p>电话: {{ phone }}</p>
+            <p>邮箱: {{ email }}</p>
+        </div>
+
         <button class="btn" @click="logout">退出登录</button>
     </div>
 </template>
@@ -10,8 +19,35 @@ import { ref, onMounted } from 'vue';
 import api from '../api';
 import { useRouter } from 'vue-router';
 
+const name = ref('');
+const gender = ref('');
+const type = ref('');
+const phone = ref('');
+const email = ref('');
 const username = ref('');
 const router = useRouter();
+
+const getUserInfo = async ()=> {
+    try {
+        const userStr = localStorage.getItem('user');
+        if(!userStr) return;
+        const user = JSON.parse(userStr);
+        console.log(user.username);
+
+        const response = await api.post('/user/userInfo', { username: user.username });
+
+        username.value = response.username;
+        name.value = response.name;
+        gender.value = response.gender;
+        type.value = response.type;
+        phone.value = response.phone; // 修正：之前写成 response.value
+        email.value = response.email; // 修正：之前写成 response.value
+    } catch( err ) {
+        console.log('获取用户信息失败', err);
+        alert('无法获取用户信息，请重新登录');
+        router.push('/login');
+    }
+}
 
 const logout = async () => {
     const token = localStorage.getItem('token');
@@ -23,11 +59,7 @@ const logout = async () => {
     }
 
     try {
-        const response = await api.post('/user/logout', { username }, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        });
+        const response = await api.post('/user/logout', { username });
 
         alert(response.message);
         localStorage.removeItem('token');
@@ -44,7 +76,8 @@ onMounted(() => {
     const userStr = localStorage.getItem('user')
     if (userStr) {
         const user = JSON.parse(userStr)
-        username.value = user.username
+        username.value = user.username;
+        getUserInfo();
     } else {
         username.value = '游客'
     }
@@ -55,5 +88,23 @@ onMounted(() => {
 .btn {
     color: white;
     background-color: #7a1a17;
+}
+
+.info {
+    display: flex;
+    background-color: #fff5f5;
+    padding: 20px;
+    border-radius: 12px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    max-width: 400px;
+    margin: 20px auto;
+    flex-direction: column;
+    
+}
+
+p {
+    font-size: 16px;
+    color: #333;
+    margin: 8px 0;
 }
 </style>
